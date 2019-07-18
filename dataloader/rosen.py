4e32b1a6ca6c50886bb6c94401dda6e151246764
mephody-bro <kirill.fedyanin@gmail.com>
2019-07-18 15:36:26 +0300

    Rosen dataset: set params on init

--- dataloader/rosen.py
+++ dataloader/rosen.py
@@ -3,20 +3,24 @@ import numpy as np
 
 
 class RosenData:
-    def __init__(self):
-        pass
+    def __init__(self, n_train, n_val, n_test, n_pool, n_dim):
+        self.n_train = n_train
+        self.n_val = n_val
+        self.n_test = n_test
+        self.n_pool = n_pool
+        self.n_dim = n_dim
 
-    def dataset(self, n_train, n_val, n_test, n_pool, n_dim):
-        X_train = np.random.random((n_train, n_dim))
+    def dataset(self):
+        X_train = np.random.random((self.n_train, self.n_dim))
         y_train = self._rosen(X_train)
 
-        X_val = np.random.random((n_val, n_dim))
+        X_val = np.random.random((self.n_val, self.n_dim))
         y_val = self._rosen(X_val)
 
-        X_test = np.random.random((n_test, n_dim))
+        X_test = np.random.random((self.n_test, self.n_dim))
         y_test = self._rosen(X_test)
 
-        X_pool = np.random.random((n_pool, n_dim))
+        X_pool = np.random.random((self.n_pool, self.n_dim))
         y_pool = self._rosen(X_pool)
 
         return X_train, y_train, X_val, y_val, X_test, y_test, X_pool, y_pool
