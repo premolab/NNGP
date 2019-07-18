1a419ee36195bdec63643e9ee45831dab306096b
mephody-bro <kirill.fedyanin@gmail.com>
2019-07-18 17:45:28 +0300

    Eager sample selector

---
+++ sample_selector/eager.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class EagerSampleSelector:
+    def update_sets(
+        self, X_train, y_train, X_pool, y_pool, ue_values, sample_size
+    ):
+        inds = self.sample(ue_values, sample_size)
+        X_train = np.concatenate([X_train, X_pool[inds]])
+        y_train = np.concatenate([y_train, y_pool[inds]])
+        X_pool = np.delete(X_pool, inds, axis=0)
+        y_pool = np.delete(y_pool, inds, axis=0)
+
+        return X_train, y_train, X_pool, y_pool
+
+    @staticmethod
+    def sample(values, sample_size):
+        return np.argsort(values)[::-1][:sample_size]
+
