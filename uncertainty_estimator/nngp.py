02f33ea4da1d0df5edd2b23b5bb395c5ef4b4ebe
mephody-bro <kirill.fedyanin@gmail.com>
2019-07-19 14:13:03 +0300

    Refactor NNGP pt.2

--- uncertainty_estimator/nngp.py
+++ uncertainty_estimator/nngp.py
@@ -11,60 +11,52 @@ class NNGP:
         self.random_subsampling = random_subsampling
         self.nn_runs = nn_runs
         self.diag_eps = diag_eps
-        self.inference_batch_size = inference_batch_size
+        self.batch_size = inference_batch_size
         self.probability = probability
         self.use_inner = use_inner
 
-        self.KK_ = tf.placeholder(tf.float32)
-        self.Q_ = tf.placeholder(tf.float32, [None, None])
-        self.K_train_cov_inv_ = tf.placeholder(tf.float32, [None, None])
-        define_one = tf.matmul(tf.transpose(self.Q_), self.K_train_cov_inv_)  # what is this?
-        define_two = tf.matmul(define_one, self.Q_)                           # and what is that?
-        self.gpue_tf = tf.linalg.tensor_diag_part(self.KK_ - define_two)
-
+        self.gpue_tf = self._build_ue_computation_graph()
 
     def estimate(self, session, X_train, y_train, X_pool):
         # data preparation
-        train_pool_samples, train_len = self._pool(X_train, X_pool)
+        train_pool_samples, train_len = self._generate_samples(X_train, X_pool)
 
-        # nn inference
-        mcd_realizations = np.zeros((train_pool_samples.shape[0], self.nn_runs))
-        for nn_run in range(self.nn_runs):
-            prediction = self._net_predict(session, train_pool_samples)
-            mcd_realizations[:, nn_run] = np.ravel(prediction)
+        # monte-carlo dropout nn inference
+        mcd_predictions = self._mcd_predict(session, train_pool_samples)
 
         # covariance matrix with regularization
-        cov_matrix_train = np.cov(mcd_realizations[:train_len, :], ddof=0)
+        cov_matrix_train = np.cov(mcd_predictions[:train_len, :], ddof=0)
         cov_matrix_inv = np.linalg.inv(cov_matrix_train + np.eye(train_len)*self.diag_eps)
 
         gp_ue = np.zeros((len(X_pool), ))
 
-        cnt = 0 # for the case of inference_batch_size > len(X_pool)
-        for cnt in range(len(X_pool) // self.inference_batch_size):
-            left_ind = train_len + cnt*self.inference_batch_size
-            right_ind = train_len + cnt*self.inference_batch_size+self.inference_batch_size
-            pool_samples = mcd_realizations[left_ind:right_ind,:]
-            Qs = self.simple_covs(mcd_realizations[:train_len,:], pool_samples).T
+        for i in range(train_len, train_len+len(X_pool), self.batch_size):
+            left = i
+            right = min(i+self.batch_size, len(mcd_predictions))
+
+            pool_samples = mcd_predictions[left:right, :]
+            Qs = self.simple_covs(mcd_predictions[:train_len, :], pool_samples).T
             KKs = np.var(pool_samples, axis=1)
-            ws = session.run(self.gpue_tf, {
-                             self.Q_: Qs,
-                             self.K_train_cov_inv_: cov_matrix_inv,
-                             self.KK_: KKs})
+
+            feed_dict = {
+                 self.Q_: Qs,
+                 self.K_train_cov_inv_: cov_matrix_inv,
+                 self.KK_: KKs
+            }
+            ws = session.run(self.gpue_tf, feed_dict)
+
             gp_ue_currents = [0 if w < 0 else np.sqrt(w) for w in np.ravel(ws)]
-            gp_ue[(left_ind - train_len):(right_ind - train_len)] = gp_ue_currents
-        right_ind = train_len + cnt*self.inference_batch_size+self.inference_batch_size
-        pool_samples = mcd_realizations[right_ind:,:]
-        Qs = self.simple_covs(mcd_realizations[:train_len,:], pool_samples).T
-        KKs = np.var(pool_samples, axis=1)
-        ws = session.run(self.gpue_tf, {
-                             self.Q_: Qs,
-                             self.K_train_cov_inv_: cov_matrix_inv,
-                             self.KK_: KKs})
-        gp_ue_currents = [0 if w < 0 else np.sqrt(w) for w in np.ravel(ws)]
-        gp_ue[right_ind:] = gp_ue_currents
+            gp_ue[(left - train_len):(right - train_len)] = gp_ue_currents
         return np.ravel(gp_ue)
 
-    def _pool(self, X_train, X_pool):
+    def _build_ue_computation_graph(self):
+        self.KK_ = tf.placeholder(tf.float32)
+        self.Q_ = tf.placeholder(tf.float32, [None, None])
+        self.K_train_cov_inv_ = tf.placeholder(tf.float32, [None, None])
+        qt_K_q = tf.matmul(tf.matmul(tf.transpose(self.Q_), self.K_train_cov_inv_), self.Q_)
+        return tf.linalg.tensor_diag_part(self.KK_ - qt_K_q)
+
+    def _generate_samples(self, X_train, X_pool):
         train_len = len(X_train)
         if self.random_subsampling > 0:
             train_len = min(self.random_subsampling, train_len)
@@ -75,11 +67,19 @@ class NNGP:
 
         return train_pool_samples, train_len
 
+    def _mcd_predict(self, session, train_pool_samples):
+        mcd_predictions = np.zeros((train_pool_samples.shape[0], self.nn_runs))
+        for nn_run in range(self.nn_runs):
+            prediction = self._net_predict(session, train_pool_samples)
+            mcd_predictions[:, nn_run] = np.ravel(prediction)
+        return mcd_predictions
+
     def _net_predict(self, session, train_pool_samples):
-        probabitily_inner = self.probability if self.use_inner else 1.
+        probability_inner = self.probability if self.use_inner else 1.
+        
         return self.net.predict(
             session, data=train_pool_samples, probability=self.probability,
-            probabitily_inner=probabitily_inner
+            probabitily_inner=probability_inner
         )
     
     @staticmethod
