f7413e9d898f9c57369b72fb153af1cef440fede
mephody-bro <kirill.fedyanin@gmail.com>
2019-07-31 15:33:29 +0300

    Reproducible code

--- uncertainty_estimator/mcdue.py
+++ uncertainty_estimator/mcdue.py
@@ -5,13 +5,16 @@ class MCDUE:
     def __init__(self, net):
         self.net = net
 
-    def estimate(self, session, X_pool, nn_runs=25, probability=.5, use_inner=False):
+    def set_session(self, session):
+        self.session = session
+
+    def estimate(self, X_pool, nn_runs=25, probability=.5, use_inner=False):
         mcd_realizations = np.zeros((X_pool.shape[0], nn_runs))
         probability_inner = probability if use_inner else 1.
         
         for nn_run in range(nn_runs):
             prediction = self.net.predict(
-                session, data=X_pool, probability=probability, probabitily_inner=probability_inner
+                self.session, data=X_pool, probability=probability, probabitily_inner=probability_inner
             )
             mcd_realizations[:, nn_run] = np.ravel(prediction)
             
