1e1e65c6c3e1310e91dadb095cc54c92fdbde678
mephody-bro <kirill.fedyanin@gmail.com>
2019-08-01 12:54:10 +0300

    MCDUE/NNGP/Random together

--- uncertainty_estimator/mcdue.py
+++ uncertainty_estimator/mcdue.py
@@ -2,19 +2,19 @@ import numpy as np
 
 
 class MCDUE:
-    def __init__(self, net):
+    def __init__(self, net, nn_runs=25, probability=.5, use_inner=False):
         self.net = net
+        self.nn_runs = nn_runs
+        self.probability = probability
+        self.use_inner = use_inner
 
-    def set_session(self, session):
-        self.session = session
-
-    def estimate(self, X_pool, nn_runs=25, probability=.5, use_inner=False):
-        mcd_realizations = np.zeros((X_pool.shape[0], nn_runs))
-        probability_inner = probability if use_inner else 1.
+    def estimate(self, X_pool, X_train, y_train):
+        mcd_realizations = np.zeros((X_pool.shape[0], self.nn_runs))
+        probability_inner = self.probability if self.use_inner else 1.
         
-        for nn_run in range(nn_runs):
+        for nn_run in range(self.nn_runs):
             prediction = self.net.predict(
-                self.session, data=X_pool, probability=probability, probabitily_inner=probability_inner
+                data=X_pool, probability=self.probability, probabitily_inner=probability_inner
             )
             mcd_realizations[:, nn_run] = np.ravel(prediction)
             
--- uncertainty_estimator/random_estimator.py
+++ uncertainty_estimator/random_estimator.py
@@ -2,5 +2,5 @@ import numpy as np
 
 
 class RandomEstimator:
-    def estimate(self, X_pool, *args, **kwargs):
+    def estimate(self, X_pool, X_train, y_train):
         return np.zeros(X_pool.shape[0])
